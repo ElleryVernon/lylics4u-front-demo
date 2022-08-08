607b64466f471792c6d0d900f78fa41463d11186
Han Seong-Min <tmpks3@gmail.com>
2022-08-08 10:44:46 +0900

    Add: artist link

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -2,12 +2,16 @@ import Image from "next/image";
 
 function MediumCard({ img, artist }) {
 	return (
-		<div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+		<a
+			href={`https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=${artist}`}
+			target="_blank"
+			className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out"
+		>
 			<div className="relative h-80 w-80">
 				<Image src={img} layout="fill" className="rounded-xl" key={img} />
 			</div>
 			<h3 className="text-2xl mt-3">{artist}</h3>
-		</div>
+		</a>
 	);
 }
 
